65889ade728d07f30b8b92152b5f5abe7971a5e4
rsg3478 <rsg3478@gmail.com>
2020-09-16 15:17:33 +0900

    메인페이지 프론트 끝

--- web/common/footer.php
+++ web/common/footer.php
@@ -7,8 +7,8 @@
             <div>
         </div>
         <div class="row">
-            <div class="col-lg-8 h-100 text-center text-lg-left my-auto">
-                <ul class="list-inline mb-2 footerfont">
+            <div class="col-lg-8 h-100 text-center text-lg-left my-auto footerfont">
+                <ul class="list-inline mb-2">
                     <li class="list-inline-item">
                         <a href="#">WCC센터</a>
                     </li>
@@ -25,33 +25,63 @@
                         <a href="#">유케이몰 고객센터</a>
                     </li>
                 </ul>
-                <div class="normal">
-                    <p class="footerfont mb-4 mb-lg-0">서울시 강남구 도산대로 207 성도빌딩 9층 (우)06026&nbsp;&nbsp;|&nbsp;&nbsp;Way to come : https://url.kr/MiU5sR</p>
-                    <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2020. All Rights Reserved.</p>
+                <div class="normal footerfont105">
+                    <p class="mb-4 mb-lg-0">서울시 강남구 도산대로 207 성도빌딩 9층 (우)06026&nbsp;&nbsp;|&nbsp;&nbsp;<a href="https://url.kr/MiU5sR" target="_blank">오시는길</a></p>
+                    <p class="mb-4 mb-lg-0">E-mail : <a href="mailto:info@uke.global">info@uke.global</a></p>
+                    <p class="mb-4 mb-lg-0">Fax : 02-573-0852</p>
+                    <p class="mb-4 mb-lg-0">Tel : <a href='tel:070-4458-5998'>070-4458-5998</a></p>
+                    <p class="mb-4 mb-lg-0">Office view : <a href="http://14.49.36.74/workplace02.jpg" target="_blank">http://14.49.36.74/workplace.jpg</a></p>
+                    <p class="text-muted small mb-4 mb-lg-0">&copy; koreanchessgame 2020. All Rights Reserved.</p>
                 </div>                
             </div>
-            <div class="col-lg-4 h-100 text-center text-lg-right my-auto">
-                <ul class="list-inline mb-0">
-                    <li class="list-inline-item mr-3">
-                        <a href="#">
-                            <i class="fab fa-facebook fa-2x fa-fw"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item mr-3">
-                        <a href="#">
-                            <i class="fab fa-twitter-square fa-2x fa-fw"></i>
-                        </a>
-                    </li>
-                    <li class="list-inline-item">
-                        <a href="#">
-                            <i class="fab fa-instagram fa-2x fa-fw"></i>
-                        </a>
-                    </li>
-                </ul>
+            <div class="col-lg-4 h-100 text-center text-lg-left my-auto footerfont">
+                <p class="mb-4 mb-lg-0">APK 다운받기</p>
+                <hr class="footerhr">
+                <div class="normal footerfont105">
+                    <!-- ㅌ[이블 만ㄷ르기 -->
+                    <table class="table table-borderless footerfont">
+                        <tbody>
+                            <tr>
+                                <td><a href="https://wcc-kr.uke.co.kr/home/apk.apk">WCC APK</a></td>
+                                <td class="downloadbtn"><a href="https://wcc-kr.uke.co.kr/home/apk.apk"><img src="<?php echo UKE_IMG_URL?>/main/download.png"/></a></td>
+                            </tr>
+                            <tr>
+                                <td><a href="http://www.uke.co.kr/ukewallet.apk">ATM APK</a></td>
+                                <td class="downloadbtn"><a href="http://www.uke.co.kr/ukewallet.apk"><img src="<?php echo UKE_IMG_URL?>/main/download.png"/></a></td>
+                            </tr>
+                            <tr>
+                                <td><a href="http://chc.uke.co.kr/home/chc.apk">CHC APK</a></td>
+                                <td class="downloadbtn"><a href="http://chc.uke.co.kr/home/chc.apk"><img src="<?php echo UKE_IMG_URL?>/main/download.png"/></a></td>
+                            </tr>
+                            <tr>
+                                <td><a data-toggle="modal" data-target="#button01">GEC APK</a></td>
+                                <td class="downloadbtn"><a data-toggle="modal" data-target="#button01"><img src="<?php echo UKE_IMG_URL?>/main/download.png"/></a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>    
             </div>
         </div>
     </div>
 </footer>
+
+<div class="modal fade" id="button01" role="dialog">
+    <div class="modal-dialog">
+        <!-- Modal content-->
+        <div class="modal-content">
+            <!-- <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">돈버는 웹툰</h4>
+            </div> -->
+            <div class="modal-body talign_c">
+              서비스준비중
+            </div>
+            <!-- <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div> -->
+        </div>
+    </div>
+</div>
 
 <!-- Bootstrap core JavaScript -->
 <!-- <script src="vendor/jquery/jquery.min.js"></script>
